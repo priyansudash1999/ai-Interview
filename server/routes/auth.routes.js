b45226cd25d13eb1c5003ee762eabaf136c58917
priyansudash1999 <1999priyansudash@gmail.com>
2026-03-26 08:20:51 +0530

    fix(typo): typo in auth.routes

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,7 +3,7 @@ import { googleAuth, logout } from "../controllers/auth.controllers.js"
 
 const authRouter = express.Router()
 
-authRouter.post("/google", googleAuth)\
+authRouter.post("/google", googleAuth)
 authRouter.get("/logout", logout)
 
 export default authRouter
